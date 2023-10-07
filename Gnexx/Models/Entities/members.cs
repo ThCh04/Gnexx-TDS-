b2249eda5dd76dd6b9f93e603761b94be57cc56d
Richard Antuan Velez Silva <20210731@itla.edu.do>
2023-10-06 23:26:38 -0400

    crearon las tablas members y team

---
+++ Gnexx/Models/Entities/members.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mail;
+
+namespace Gnexx.Models.Entities
+{
+    public class members
+    {
+        [Key]
+        public int Id { get; set; }
+        public int TeamId { get; set; }
+        public int MemberId { get; set; }
+        public string MemberName { get; set; }
+
+
+    }
+}
+
+
---
+++ Gnexx/Models/Entities/team.cs
@@ -0,0 +1,15 @@
+﻿using Gnexx.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gnexx.Models.Entities
+{
+    public class team
+    {
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public string UserName { get; set; }
+        public string Campo { get; set; }
+    }
+}
+
+
