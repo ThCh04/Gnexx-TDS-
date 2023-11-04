ef0f7720deaa661906e382e8cacbc44515a7c315
Isael Diroche <isaeldiroche00@gmail.com>
2023-11-04 11:35:32 -0400

    Gnexx: esqueleto del formulario de registro

---
+++ Gnexx/wwwroot/js/Auth/script_register.js
@@ -0,0 +1,24 @@
+﻿
+function mostrarSeccion(seccionId) {
+    var secciones = document.getElementsByClassName('section-registro');
+    for (var i = 0; i < secciones.length; i++) {
+        secciones[i].style.display = 'none';
+    }
+    document.getElementById(seccionId).style.display = 'block';
+}
+
+function validarYMostrar(seccionActual, seccionSiguiente) {
+    var camposValidos = true;
+    var campos = document.querySelectorAll('#' + seccionActual + ' input');
+    for (var i = 0; i < campos.length; i++) {
+        if (campos[i].value.trim() === '') {
+            camposValidos = false;
+            alert('Por favor, complete todos los campos antes de continuar.');
+            break;
+        }
+    }
+
+    if (camposValidos) {
+        mostrarSeccion(seccionSiguiente);
+    }
+}
