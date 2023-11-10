b7fb1e89ac369a81bcf609b98005eb631a441818
Richard Antuan Velez Silva <20210731@itla.edu.do>
2023-11-09 20:29:21 -0400

    Añadir a la BD las entidades para el manejo de los mensajes, postulaciones y las interacciones.

---
+++ Gnexx.Repository/Context/Interaciones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gnexx.Repository.Context
+{
+    public class Interaccion
+    {
+        public int InteraccionId { get; set; }
+        public string Tipo { get; set; }
+        public DateTime FechaInteraccion { get; set; }
+
+        // Relaciones
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+
+}
