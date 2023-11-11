f673b720ad81220fa235a494b6406a21a022ce60
sebastian de leon <sebastiandeleonrosario@gmail.com>
2023-11-10 21:16:40 -0500

    Generic repositories

--- Gnexx.Services/Repos/NewsRepo.cs
+++ Gnexx.Services/Repos/NewsRepo.cs
@@ -1,6 +1,8 @@
 ﻿using Gnexx.Models.Entities;
 using Gnexx.Repository;
+using Gnexx.Repository.Context;
 using Gnexx.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,24 +11,75 @@ using System.Threading.Tasks;
 
 namespace Gnexx.Services.Repos
 {
-    public class NewsRepo : INewsRepo
+    public class NewsRepo<T> : INewsRepo<T> where T : class
     {
 
-        private readonly IRepository<News> _repository;
+        private readonly GnexxDbContext _context;
+        private readonly DbSet<T> entity;
 
-        public NewsRepo(IRepository<News> repository)
+        public NewsRepo(GnexxDbContext context)
         {
-            _repository = repository;
+            _context = context;
+            entity = _context.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return entity.AsEnumerable();
         }
 
-        public IEnumerable<News> GetAll()
+        public T FindById(int ID)
         {
-            return _repository.GetAll();
+            return entity.FirstOrDefault(FindById(ID));
         }
 
-        public News FindById(int ID)
+        #region Create
+        public virtual async Task CreateAsync(T entity)
         {
-            return _repository.FindById(ID);
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+        #endregion
+
+        #region Read
+        public virtual async Task<List<T>> GetAllAsync()
+        {
+            return await entity.ToListAsync();
+        }
+        public async Task<List<T>> GetAllWithInclude(List<string> properties)
+        {
+            var query = entity.AsQueryable();
+            foreach (string property in properties)
+            {
+                query = query.Include(property);
+            }
+            return await query.ToListAsync();
+        }
+        public virtual async Task<T> GetById(int id)
+        {
+            return await entity.FindAsync(id);
+        }
+        #endregion
+
+        #region Update
+        public virtual async Task<bool> UpdateAsync(T entity, int id)
+        {
+            T entry = await this.entity.FindAsync(id);
+            this.entity.Entry(entry).CurrentValues.SetValues(entity);
+            return await Save();
+        }
+        #endregion
+
+        #region Delete
+        public virtual async Task<bool> DeleteAsync(T entity)
+        {
+            entity.Remove(entity);
+            return await Save();
+        }
+        #endregion
+
+        private async Task<bool> Save()
+        {
+            return await entity.SaveChangesAsync() >= 0 ? true : false;
         }
 
 
