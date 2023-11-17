e3e77ec8ce9f4f0e9293e3cf1cb414b4375d77fc
sebastian de leon <sebastiandeleonrosario@gmail.com>
2023-11-17 10:22:59 -0500

    Seedc, services del register, ogin y servicios para mail

--- Gnexx/Program.cs
+++ Gnexx/Program.cs
@@ -1,4 +1,8 @@
+using Gnexx.Identity;
+using Gnexx.Identity.Entities;
+using Gnexx.Identity.Seeds;
 using Gnexx.Repository.Context;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,12 +11,32 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddIdentityInfrastructure(builder.Configuration);
 builder.Services.AddDbContext<GnexxDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("Gnexx.Repository")
     ));
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var service = scope.ServiceProvider;
+    try
+    {
+        var userManager = service.GetRequiredService<UserManager<ApplicationUser>>();
+        var userRoles = service.GetRequiredService<RoleManager<IdentityRole>>();
+
+        await DefaultRoles.SeedAsync(userManager, userRoles);
+        await DefaultCoachuser.SeedAsync(userManager, userRoles);
+        await DefaultPlayerUser.SeedAsync(userManager, userRoles);
+        
+        //await DefaultPropertyType.SeedAsync(propertyType);
+    }
+    catch (Exception ex)
+    {
+        throw;
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -27,6 +51,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
