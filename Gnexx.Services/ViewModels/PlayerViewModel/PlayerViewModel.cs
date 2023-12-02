26f802e85dfc2472e37bc1944a5b65d27666afa1
DwanilSanchez30 <dwanilsanchez01@gmail.com>
2023-12-01 22:01:01 -0400

    Creacion de ViewModels
    Creacion de viewModels de coach, comments, player y team

---
+++ Gnexx.Services/ViewModels/PlayerViewModel/PlayerViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gnexx.Services.ViewModels.PlayerViewModel
+{
+    public class PlayerViewModel
+    {
+
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string P_Username { get; set; }
+
+        [Required]
+        public string P_Name { get; set; }
+
+        [Required]
+        public string P_Lastname { get; set; }
+
+        [Required]
+        public string P_Nickname { get; set; }
+
+        [Required]
+        public DateTime P_Datebirth { get; set; }
+
+        [Required]
+        public string P_Specialization { get; set; }
+
+        [Required]
+        public string P_Description { get; set; }
+
+        [Required]
+        public string P_Contact { get; set; }
+        public int TeamID { get; set; }
+        public int UserID { get; set; }
+
+    }
+}
