a0749a116156b86282c3a48b952bb8ffdabff8f3
sebastian de leon <sebastiandeleonrosario@gmail.com>
2023-12-14 22:09:09 -0500

    PostService

---
+++ Gnexx.Services/Interfaces/Services/IPostService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gnexx.Services.Interfaces.Services
+{
+    internal interface IPostService
+    {
+    }
+}
