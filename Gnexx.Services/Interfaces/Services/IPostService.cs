a8ad26f974fcde254904755f642d45668070b96b
Eduard Cruz <edward-cruz@hotmail.com>
2023-12-14 23:48:05 -0400

    Revert "Service Postulation2"
    This reverts commit 7795ebd2a290c0143e9d34f5c8f0ac7a32d8c2f1.

--- Gnexx.Services/Interfaces/Services/IPostService.cs
+++ Gnexx.Services/Interfaces/Services/IPostService.cs
@@ -1,10 +1,12 @@
-﻿using Gnexx.Data.Entities;
-using Gnexx.Services.Interfaces.Repository;
-using Gnexx.Services.ViewModels.PostulationViewModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Gnexx.Services.Interfaces.Services
 {
-    public interface IPostService : IGenericService<PostulationViewModel>
+    internal interface IPostService
     {
     }
 }
--- Gnexx.Services/Services/PostService.cs
+++ Gnexx.Services/Services/PostService.cs
@@ -1,28 +1,12 @@
-﻿using AutoMapper;
-using Gnexx.Data.Entities;
-using Gnexx.Services.DTOs.Account;
-using Gnexx.Services.Helpers;
-using Gnexx.Services.Interfaces.Repository;
-using Gnexx.Services.Interfaces.Services;
-using Gnexx.Services.ViewModels.PostulationViewModel;
-using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Gnexx.Services.Services
 {
-    public class PostService : GenericService<PostulationViewModel,Postulation>,IPostService
+    internal class PostService
     {
-        private readonly IPostRepo _postRepo;
-        private readonly IMapper _mapper;
-        private readonly IHttpContextAccessor _httpContext;
-        private readonly AuthenticationResponse response;
-
-        public PostService(IPostRepo postRepo, IMapper mapper, IHttpContextAccessor httpContext): base(postRepo,mapper)
-        {
-            _postRepo = postRepo;
-            _mapper = mapper;
-            _httpContext = httpContext;
-
-            response = _httpContext.HttpContext.Session.Get<AuthenticationResponse>("user");
-        }
     }
 }
